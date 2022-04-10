a6eaeda3e9da4b440f37a6450863fd794e4194a6
sihle <gubug777@gmail.com>
2022-04-10 15:17:16 +0800

    add Sidebar Component

---
+++ src/components/SidebarComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="bg-white border border-2 border-dark px-6 py-8">
+    <a href="#" class="btn btn-primary shadow-btn w-100 mb-6">張貼動態</a>
+    <ul>
+      <li class="d-flex align-items-center">
+        <img class="icons me-4" src="@/assets/img/user.png" alt="headshot" />
+        <b>邊緣小杰</b>
+      </li>
+      <li class="d-flex align-items-center"></li>
+      <li class="d-flex align-items-center"></li>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.icons {
+  width: 50px;
+  height: 50px;
+}
+</style>
