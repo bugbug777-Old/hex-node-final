4a740809d56a25dda1c0ec74bd80ef544a95485e
sihle <gubug777@gmail.com>
2022-04-10 16:03:56 +0800

    add sidebar menu, and import sidebar to MetawallView

--- src/components/SidebarComponent.vue
+++ src/components/SidebarComponent.vue
@@ -1,20 +1,29 @@
 <template>
   <div class="bg-white border border-2 border-dark px-6 py-8">
-    <a href="#" class="btn btn-primary shadow-btn w-100 mb-6">張貼動態</a>
-    <ul>
-      <li class="d-flex align-items-center">
-        <img class="icons me-4" src="@/assets/img/user.png" alt="headshot" />
-        <b>邊緣小杰</b>
+    <a
+      href="#"
+      class="btn btn-primary btn-hover-warning shadow-btn border border-2 border-dark w-100 mb-6"
+      >張貼動態</a
+    >
+    <ul class="menu">
+      <li class="menu-item mb-5">
+        <a class="menu-link" href="#">
+          <img class="menu-icons border-0 me-4" src="@/assets/img/user.png" alt="headshot" />
+          邊緣小杰
+        </a>
       </li>
-      <li class="d-flex align-items-center"></li>
-      <li class="d-flex align-items-center"></li>
+      <li class="menu-item mb-5">
+        <a class="menu-link" href="#">
+          <i class="menu-icons bg-info bi bi-bell fs-5 me-4"></i>
+          追蹤名單
+        </a>
+      </li>
+      <li class="menu-item">
+        <a class="menu-link" href="#">
+          <i class="menu-icons bg-info bi bi-hand-thumbs-up fs-5 me-4"></i>
+          追蹤名單
+        </a>
+      </li>
     </ul>
   </div>
 </template>
-
-<style lang="scss" scoped>
-.icons {
-  width: 50px;
-  height: 50px;
-}
-</style>
--- src/views/MetawallView.vue
+++ src/views/MetawallView.vue
@@ -1,14 +1,23 @@
 <template>
   <Navbar />
-  <div class="container">Meta Wall</div>
+  <div class="container pt-12" style="max-width: 869px">
+    <div class="d-flex justify-content-between">
+      <div style="width: 61%">Content Wall</div>
+      <div style="width: 35%">
+        <Sidebar />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import Navbar from '@/components/NavbarComponent.vue';
+import Sidebar from '@/components/SidebarComponent.vue';
 
 export default {
   components: {
     Navbar,
+    Sidebar,
   },
 };
 </script>
